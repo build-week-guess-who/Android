d58dc9ca9dbefd0f25d1c10073b26bc2cd8975a1
gcgsauce <gattigcg@gmail.com>
2019-08-27 06:03:16 +0800

    FIRST COMMIT

---
+++ app/src/main/java/com/example/twitterpresidents/CandidateChoiceListAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.twitterpresidents
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.twitterpresidents.model.PresidentialCandidate
+
+class CandidateChoiceListAdapter(val data : MutableList<PresidentialCandidate>) : RecyclerView.Adapter<CandidateChoiceListAdapter.ViewHolder>() {
+
+    class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(parent)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+}
