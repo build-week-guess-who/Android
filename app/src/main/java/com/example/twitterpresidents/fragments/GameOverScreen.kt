5a074991f35d83b43abdacc143f34451f725fe5b
GCGsauce <gattigcg1@gmail.com>
2019-08-27 11:22:01 -0400

    d

--- app/src/main/java/com/example/twitterpresidents/fragments/GameOverScreen.kt
+++ app/src/main/java/com/example/twitterpresidents/fragments/GameOverScreen.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 
 import com.example.twitterpresidents.R
 
@@ -23,7 +24,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [GameOverScreen.newInstance] factory method to
  * create an instance of this fragment.
  */
-class GameOverScreen : Fragment() {
+class GameOverScreen : DialogFragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
