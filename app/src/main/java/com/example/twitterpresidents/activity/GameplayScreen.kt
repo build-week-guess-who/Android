1f0a55b45b4c9cd6a137cbe5c1891d275cb43f36
GCGsauce <gattigcg1@gmail.com>
2019-08-27 12:45:19 -0400

    added a custom view which includes the tweet display + some other ui elements

--- app/src/main/java/com/example/twitterpresidents/activity/GameplayScreen.kt
+++ app/src/main/java/com/example/twitterpresidents/activity/GameplayScreen.kt
@@ -1,5 +1,10 @@
 package com.example.twitterpresidents.activity
 
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.SoundPool
+import android.net.rtp.AudioStream
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -10,10 +15,11 @@ import com.example.twitterpresidents.fragments.Progressbar
 import com.example.twitterpresidents.model.PresidentialCandidate
 import kotlinx.android.synthetic.main.activity_gameplay_screen.*
 
-//user selects who made the tweet
+//user selects who made the tweet.
+//composed of top portion which includes a lot of ui elements and the tweet in a custom view
+//bottom part is the recycler view of the candidate choices.
 
-class GameplayScreen : AppCompatActivity(), Lifebar.OnFragmentInteractionListener,
-    Progressbar.OnFragmentInteractionListener {
+class GameplayScreen : AppCompatActivity(){
 
     companion object {
         val presidentialList = mutableListOf<PresidentialCandidate>()
@@ -22,6 +28,9 @@ class GameplayScreen : AppCompatActivity(), Lifebar.OnFragmentInteractionListene
         get(){
             return (0..3).random()
         }
+        var correctSound : Int? = null
+        var wrongSound : Int? = null
+        var soundPool : SoundPool? = null
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,14 +47,23 @@ class GameplayScreen : AppCompatActivity(), Lifebar.OnFragmentInteractionListene
         presidentialList.add(PresidentialCandidate(R.drawable.question_mark_outline, "Donald Trump", "@realdonaldtrump"))
         presidentialList.add(PresidentialCandidate(R.drawable.question_mark_outline, "Kamala Harris", "@kamalaharris"))
         candidateListAdapter.notifyDataSetChanged()
-    }
 
-    override fun noMoreLives() {
-        //call game over screen...restart game
+        //setup sound effects for right or wrong answers
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            val audioAttributes = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build()
 
-    }
+            soundPool = SoundPool.Builder()
+                .setMaxStreams(6)
+                .setAudioAttributes(audioAttributes)
+                .build()
+        } else{
+            soundPool = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
+        }
 
-    override fun progressbarMaximized() {
-        //move to screen where exp is accumulated
+        correctSound = soundPool?.load(this, R.raw.correct_sound, 1)
+        wrongSound = soundPool?.load(this, R.raw.wrong_sound, 1)
     }
 }
