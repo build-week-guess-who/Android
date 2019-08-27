d37d9389c02a6689202bb25fd1a739ff878aa347
gcgsauce <gattigcg@gmail.com>
2019-08-27 15:30:56 +0800

    basic work on the gameplay screen.

--- app/src/main/java/com/example/twitterpresidents/activity/InstructionsPage.kt
+++
@@ -1,4 +0,0 @@
-package com.example.twitterpresidents.activity
-
-class InstructionsPage {
-}
--- app/src/main/java/com/example/twitterpresidents/activity/MainActivity.kt
+++ app/src/main/java/com/example/twitterpresidents/activity/MainActivity.kt
@@ -3,24 +3,22 @@ package com.example.twitterpresidents.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.twitterpresidents.R
+import com.example.twitterpresidents.R.layout.*
 import kotlinx.android.synthetic.main.activity_main.*
 
-class LaunchPage : AppCompatActivity() {
+const val LOGIN_INTENT_CODE = 1
+
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
 
         button_chose_to_login.setOnClickListener{
-            val goToLogin = Intent(this, LoginActivity::class.java)
+            val goToLogin = Intent(this@MainActivity, LoginActivity::class.java)
             goToLogin.putExtra("LOGIN_INTENT_CODE", 1)
             startActivity(goToLogin)
-
-
-    }
-        button_chose_to_sign_in.setOnClickListener{
-            val goToSignIn = Intent(this, signIn::class.java)
-            startActivity(goToSignIn)
         }
-}}
+    }
+}
+//initial commit
