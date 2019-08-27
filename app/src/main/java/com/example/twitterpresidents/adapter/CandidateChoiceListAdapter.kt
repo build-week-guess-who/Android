94707b158efb27aec3b22958604595743b9b24ec
gcgsauce <gattigcg@gmail.com>
2019-08-27 14:45:16 +0800

    finished simple list adapter

--- app/src/main/java/com/example/twitterpresidents/adapter/CandidateChoiceListAdapter.kt
+++ app/src/main/java/com/example/twitterpresidents/adapter/CandidateChoiceListAdapter.kt
@@ -1,18 +1,26 @@
 package com.example.twitterpresidents.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.example.twitterpresidents.R
 import com.example.twitterpresidents.model.PresidentialCandidate
+import kotlinx.android.synthetic.main.candidate_choice.view.*
 
 class CandidateChoiceListAdapter(val data : MutableList<PresidentialCandidate>) : RecyclerView.Adapter<CandidateChoiceListAdapter.ViewHolder>() {
 
-    class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
-
+    class ViewHolder(val view : View) : RecyclerView.ViewHolder(view) {
+        val portrait : ImageView = view.candidate_portrait
+        val details : TextView = view.candidate_details
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(parent)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.candidate_choice, parent, false)
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
@@ -20,6 +28,7 @@ class CandidateChoiceListAdapter(val data : MutableList<PresidentialCandidate>) 
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
+        holder.portrait.setImageDrawable(ContextCompat.getDrawable(holder.view.context, data[position].portraitId))
+        holder.details.text = data[position].name + "\n" + data[position].twitterHandle
     }
 }
