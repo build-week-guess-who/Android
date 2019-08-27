fd9a9cda107b530064a1ffd78fbf2d1dfe940caf
Troy Christian <apostasy8@gmail.com>
2019-08-26 20:55:13 -0700

    make LaunchPage.kt the real launch page. Set LaunchPage.kt as the activity with the .LAUNCHER in the manifest.xml, set onclicklistener that takes user to Login page upon clicking login.

--- app/src/main/java/com/example/twitterpresidents/activity/LaunchPage.kt
+++ app/src/main/java/com/example/twitterpresidents/activity/LaunchPage.kt
@@ -1,13 +1,20 @@
 package com.example.twitterpresidents.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.twitterpresidents.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class LaunchPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_log_in.setOnClickListener{
+            val goToLogin = Intent(this, loginActivity::class.java)
+            goToLogin.putExtra("LOGIN_INTENT_CODE", 1)
+            startActivity(goToLogin)
     }
-}
+}}
