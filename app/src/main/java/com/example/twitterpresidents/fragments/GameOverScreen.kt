faa8a2d735912bae0559f2b23df5325258c08842
GCGsauce <gattigcg1@gmail.com>
2019-08-28 00:42:29 -0400

    Finished mock up of game over screen.

--- app/src/main/java/com/example/twitterpresidents/fragments/GameOverScreen.kt
+++ app/src/main/java/com/example/twitterpresidents/fragments/GameOverScreen.kt
@@ -1,6 +1,7 @@
 package com.example.twitterpresidents.fragments
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -10,32 +11,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 
 import com.example.twitterpresidents.R
+import com.example.twitterpresidents.activity.ModeSelection
+import kotlinx.android.synthetic.main.fragment_game_over_screen.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [GameOverScreen.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [GameOverScreen.newInstance] factory method to
- * create an instance of this fragment.
- */
+//screen that asks whether the user wants to continue
 class GameOverScreen : DialogFragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -43,61 +27,18 @@ class GameOverScreen : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_over_screen, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_game_over_screen, container, false)
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        } else {
-            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        continue_yes.setOnClickListener {
+            val intent = Intent(activity as Context, ModeSelection::class.java)
+            startActivity(intent)
         }
-    }
 
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
+        continue_no.setOnClickListener {
+            activity?.finish()
+            System.exit(0)
+        }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GameOverScreen.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GameOverScreen().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        return view
     }
 }
