09766e4d9239a6a930d09a67457c04b174fd7fc3
Troy Christian <apostasy8@gmail.com>
2019-08-28 21:59:44 -0700

    progress bar test animation is complete

--- app/src/main/java/com/example/twitterpresidents/fragments/Progressbar.kt
+++ app/src/main/java/com/example/twitterpresidents/fragments/Progressbar.kt
@@ -8,8 +8,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 
 import com.example.twitterpresidents.R
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_gameplay_screen.*
 import kotlinx.android.synthetic.main.fragment_progressbar.*
 
 //fragment that encapsulates the progress bar
@@ -18,14 +21,15 @@ class Progressbar : Fragment() {
 
 //    var instance : Progressbar? = null
     var listener: OnFragmentInteractionListener? = null
-    var progress = 0
-
+   // var progress = 0
+  // val progressBar = R.id.progressBar as ProgressBar
     interface OnFragmentInteractionListener {
         fun progressbarMaximized()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -33,24 +37,26 @@ class Progressbar : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_progressbar, container, false)
-//        instance = view as Progressbar?
-//
-//        return view
-    }
+        return inflater.inflate(R.layout.fragment_progressbar, container, false)}
+
 
     //extends progress bar to signify when a correct answer has been given.
     fun extendProgressbar(){
-        progress += 1
-        //progress_text.text = "Progress " + progress.toString()
+        progressBar.progress = 0
+        progressBar.setOnClickListener{
+            progressBar.progress = (progressBar.progress + 1)
+            //if progress bar is clicked just so, it will animate. 5 total clicks to fill bar once activated.
+        }
     }
+
+
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
+        if (context is Progressbar.OnFragmentInteractionListener) {
             listener = context
         } else {
             throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
         }
-    }
-}
+    }}
+
